1b9e376d14e5b1a30d15a6d51f2e6f9d4a9c3953
Richard Bachmann <shoewielder@telenordigital.com>
2018-08-15 15:15:05 +0200

    Fixed missing oauth2 scopes

--- bridge-terraform/modules/network-terraform/main.tf
+++ bridge-terraform/modules/network-terraform/main.tf
@@ -80,6 +80,12 @@ resource "google_container_cluster" "splunk-fwd" {
   subnetwork = "${google_compute_subnetwork.minimal-network.self_link}"
   initial_node_count = "${var.node_count}"
   node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
     tags = ["${var.gke_node_tag}"]
   }
   depends_on = ["google_compute_subnetwork.minimal-network"]
